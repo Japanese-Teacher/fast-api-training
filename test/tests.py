59fc024165d44fae8c4a7013489e28249b2365a2
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-04-21 21:09:06 +0300

    style: added annotations to functions and replaced magic value 200 with parameter

--- test/tests.py
+++ test/tests.py
@@ -1,12 +1,13 @@
 from http import client
 
 
-def test_foo():
+def test_foo() -> None:
     assert True
 
 
-def test_add_book_success():
+def test_add_book_success() -> None:
+    status_code_success = 200
     new_book = {"id": 3, "title": "New Book", "author": "New Author"}
     response = client.post("/books", json=new_book)
-    assert response.status_code == 200
+    assert response.status_code == status_code_success
     assert response.json()["title"] == "New Book"
