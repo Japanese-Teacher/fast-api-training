46003da4a84b9775da2acc69b946f9fd9932992b
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-04-22 17:47:05 +0300

    feat: imported client to tests

--- test/tests.py
+++ test/tests.py
@@ -1,13 +1,15 @@
-from http import client
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
 
 
 def test_foo() -> None:
     assert True
 
-
 def test_add_book_success() -> None:
-    status_code_success = 200
     new_book = {"id": 3, "title": "New Book", "author": "New Author"}
     response = client.post("/books", json=new_book)
-    assert response.status_code == status_code_success
+
+    assert response.status_code == 200
     assert response.json()["title"] == "New Book"
