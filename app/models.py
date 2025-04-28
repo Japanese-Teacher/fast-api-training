d6abd3511fcc7b9a7b2d87eb5d63a01391b34a55
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-04-28 21:05:18 +0300

    feat: delted author from class Book

--- app/models.py
+++ app/models.py
@@ -4,9 +4,8 @@ from pydantic import BaseModel
 class Book(BaseModel):
     id: int
     title: str
-    author: str
 
 
 books_db = [
-    Book(id=1, title="Sample Book", author="Sample Author")
+    Book(id=1, title="Sample Book")
 ]
