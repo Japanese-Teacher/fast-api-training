6f00573835bde335b7923d9b3752cab9d02c97be
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-05-20 01:45:11 +0300

    feat: added more parameters for BookORM

--- app/orm.py
+++ app/orm.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from mypyc.ir.ops import Integer
-from sqlalchemy import create_engine, String, select
+from sqlalchemy import create_engine, String, select, ForeignKey, text
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped
 from sqlalchemy.testing.schema import mapped_column
 
@@ -10,8 +12,14 @@ class BaseORM(DeclarativeBase):
 
 class BookORM(BaseORM):
     def __repr__(self):
-        return f"BookORM(id={self.id}, name={self.name})"
+        return f"BookORM(id={self.id}, name={self.name}, description={self.description})"
 
     __tablename__ = "books"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    author_name: Mapped[str] = mapped_column(ForeignKey("authors.name", ondelete="CASCADE"), nullable=False)
+    publisher_name: Mapped[str] = mapped_column(ForeignKey("publishers.name"), nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True, default=None)
+    created_at: Mapped[datetime] = mapped_column(server_default=text("now()"), nullable = False)
+    updated_at: Mapped[datetime] = mapped_column(nullable = False)
+    deleted_at: Mapped[datetime | None] = mapped_column(nullable = True)
