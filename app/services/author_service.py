3704a9bb7927bbdc02a3a4e4db1eb46184abb7f2
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-05-31 22:16:22 +0300

    feat: created 3 endpoints to authors

--- app/services/author_service.py
+++ app/services/author_service.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import Depends
 
 from app.integrations.postgres.author_repository import AuthorRepository
-from app.models import AuthorDTO
+from app.models import AuthorDTO, NewAuthorDTO
 
 
 class AuthorService:
@@ -15,3 +15,23 @@ class AuthorService:
 
     def get_authors(self) -> list[AuthorDTO]:
         return self.author_repository.get_authors()
+
+    def add_author(
+            self,
+            author_dto: AuthorDTO,
+    ) -> AuthorDTO:
+        return self.author_repository.add_author(author_dto)
+
+    def update_author(
+            self,
+            author_name: str,
+            new_author_dto: NewAuthorDTO,
+    ) -> NewAuthorDTO:
+        return self.author_repository.update_author(author_name, new_author_dto)
+
+    def delete_author(
+            self,
+            author_name: str,
+    ) -> str:
+        return self.author_repository.delete_author(author_name)
+
--- app/transport/handlers/authors.py
+++ app/transport/handlers/authors.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-from app.models import AuthorDTO
+from app.models import AuthorDTO, NewAuthorDTO
 from app.services.author_service import AuthorService
 
 author_router = APIRouter(
@@ -14,5 +14,26 @@ author_router = APIRouter(
 async def get_authors(
         author_service: Annotated[AuthorService, Depends(AuthorService)]
 ) -> list[AuthorDTO]:
-    result = author_service.get_authors()
-    return result
+    return author_service.get_authors()
+
+@author_router.post('', response_model=AuthorDTO)
+async def add_author(
+        author_service: Annotated[AuthorService, Depends(AuthorService)],
+        author_dto: AuthorDTO
+) -> AuthorDTO:
+    return author_service.add_author(author_dto)
+
+@author_router.put('', response_model=NewAuthorDTO)
+async def update_author(
+        author_service: Annotated[AuthorService, Depends(AuthorService)],
+        author_name: str,
+        new_author_dto: NewAuthorDTO,
+) -> NewAuthorDTO:
+    return author_service.update_author(author_name, new_author_dto)
+
+@author_router.delete('', response_model=str)
+async def delete_author(
+        author_service: Annotated[AuthorService, Depends(AuthorService)],
+        author_name: str,
+) -> str:
+    return author_service.delete_author(author_name)
