1bdf70138b06d266e0cbdeed6b358e11523eb2ca
Japanese-Teacher <leva.kostetskiy13@mail.ru>
2025-06-01 20:27:30 +0300

    feat: created endpoints to users

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 from pydantic import BaseModel
 
+
 class AuthorDTO(BaseModel):
     name: str
     nationality: str
     date_of_birth: datetime
+
 
 class BookDTO(BaseModel):
     id: int
@@ -14,6 +16,7 @@ class BookDTO(BaseModel):
     publisher_name: str
     description: str | None
 
+
 class NewBookDTO(BaseModel):
     id: int
     new_authors: list[AuthorDTO]
@@ -21,13 +24,34 @@ class NewBookDTO(BaseModel):
     new_publisher_name: str
     new_description: str | None
 
+
 class NewAuthorDTO(BaseModel):
     new_name: str
     new_nationality: str
     new_date_of_birth: datetime
 
+
 class PublisherDTO(BaseModel):
     name: str
 
+
 class NewPublisherDTO(BaseModel):
     name: str
+
+
+class UserDTO(BaseModel):
+    id: int
+    first_name: str
+    second_name: str
+    age: int
+    login: str
+    password_hash: str
+
+
+class NewUserDTO(BaseModel):
+    id: int
+    new_first_name: str
+    new_second_name: str
+    new_age: int
+    new_login: str
+    new_password_hash: str
